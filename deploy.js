de2a98bcfef9bf118b80de5861a139cf855e0df8
iarmankhan <work.armankhan@gmail.com>
2022-09-10 13:41:30 +0530

    :rocket: Deploying contract using ethers js

--- deploy.js
+++ deploy.js
@@ -1,4 +1,32 @@
-async function main() {}
+const ethers = require("ethers");
+const fs = require("fs");
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+
+  const wallet = new ethers.Wallet(
+    "4721b3b471ffacfcd877af31b2989b88766af5517644cd7e45d037040c55eb79",
+    provider
+  );
+
+  const abi = fs.readFileSync(
+    "./out/contracts_SimpleStorage_sol_SimpleStorage.abi",
+    "utf8"
+  );
+
+  const binary = fs.readFileSync(
+    "./out/contracts_SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+
+  const contract = await contractFactory.deploy();
+  console.log(contract);
+}
 
 main()
   .then(() => process.exit(0))
