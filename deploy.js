0588470e0cfc9dc3c3f41e3f6faddaea767e741f
iarmankhan <work.armankhan@gmail.com>
2022-09-11 19:56:48 +0530

    :sparkles: deploying to testnet

--- deploy.js
+++ deploy.js
@@ -5,20 +5,20 @@ const fs = require("fs");
 async function main() {
     const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 
-    // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
-    let wallet = new ethers.Wallet.fromEncryptedJson(
-        encryptedJson,
-        process.env.PRIVATE_KEY_PASSWORD
-    );
-
-    wallet = (await wallet).connect(provider);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+    // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+    // let wallet = new ethers.Wallet.fromEncryptedJson(
+    //     encryptedJson,
+    //     process.env.PRIVATE_KEY_PASSWORD
+    // );
+    //
+    // wallet = (await wallet).connect(provider);
 
     const abi = fs.readFileSync(
         "./out/contracts_SimpleStorage_sol_SimpleStorage.abi",
         "utf8"
     );
- 
+
     const binary = fs.readFileSync(
         "./out/contracts_SimpleStorage_sol_SimpleStorage.bin",
         "utf8"
@@ -29,6 +29,7 @@ async function main() {
 
     const contract = await contractFactory.deploy();
     await contract.deployTransaction.wait(1);
+    console.log(`Contract address: ${contract.address}`)
 
     const currentFavoriteNumber = await contract.retrieve();
     console.log("Current favorite number:", currentFavoriteNumber.toString());
