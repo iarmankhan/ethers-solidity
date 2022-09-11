b421934007dff4508ebef421a27e5c770d56b812
iarmankhan <work.armankhan@gmail.com>
2022-09-11 12:08:15 +0530

    :sparkles: Encrypt private key and use that for transactions

--- deploy.js
+++ deploy.js
@@ -5,7 +5,14 @@ const fs = require("fs");
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+  let wallet = new ethers.Wallet.fromEncryptedJson(
+    encryptedJson,
+    process.env.PRIVATE_KEY_PASSWORD
+  );
+
+  wallet = (await wallet).connect(provider);
 
   const abi = fs.readFileSync(
     "./out/contracts_SimpleStorage_sol_SimpleStorage.abi",
