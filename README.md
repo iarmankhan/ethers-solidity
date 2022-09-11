a5e3af32e7ed99791d8f1ea65c4b6e7048b07ee2
iarmankhan <work.armankhan@gmail.com>
2022-09-11 12:25:57 +0530

    :sparkles: Added prettier, updated readme

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-# Solidity Basics
+# Simple Storage - Solidity Basics
 
 This repository contains some basic smart contracts created using Solidity.
 
-You can run these contracts on VM in [Remix IDE](https://remix.ethereum.org/)
+### Requirements:
+
+- Nodejs v16.x
+- Yarn v1.2.x
+- Ganache for test wallet
+
+### Steps to run:
+
+- run `yarn install`
+- Copy `.env.sample` file to `.env`
+- Replace env variables with real values, add your private key in .env
+- Generate encryptedKey using `encryptKey.js`
+- run `yarn start`
 
 Peace ✌️
--- deploy.js
+++ deploy.js
@@ -3,44 +3,44 @@ const ethers = require("ethers");
 const fs = require("fs");
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 
-  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
-  let wallet = new ethers.Wallet.fromEncryptedJson(
-    encryptedJson,
-    process.env.PRIVATE_KEY_PASSWORD
-  );
+    // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+    let wallet = new ethers.Wallet.fromEncryptedJson(
+        encryptedJson,
+        process.env.PRIVATE_KEY_PASSWORD
+    );
 
-  wallet = (await wallet).connect(provider);
+    wallet = (await wallet).connect(provider);
 
-  const abi = fs.readFileSync(
-    "./out/contracts_SimpleStorage_sol_SimpleStorage.abi",
-    "utf8"
-  );
+    const abi = fs.readFileSync(
+        "./out/contracts_SimpleStorage_sol_SimpleStorage.abi",
+        "utf8"
+    );
+ 
+    const binary = fs.readFileSync(
+        "./out/contracts_SimpleStorage_sol_SimpleStorage.bin",
+        "utf8"
+    );
 
-  const binary = fs.readFileSync(
-    "./out/contracts_SimpleStorage_sol_SimpleStorage.bin",
-    "utf8"
-  );
+    const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+    console.log("Deploying, please wait...");
 
-  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  console.log("Deploying, please wait...");
+    const contract = await contractFactory.deploy();
+    await contract.deployTransaction.wait(1);
 
-  const contract = await contractFactory.deploy();
-  await contract.deployTransaction.wait(1);
-
-  const currentFavoriteNumber = await contract.retrieve();
-  console.log("Current favorite number:", currentFavoriteNumber.toString());
-  const transactionResponse = await contract.store("7");
-  await transactionResponse.wait(1);
-  const newFavoriteNumber = await contract.retrieve();
-  console.log("New favorite number:", newFavoriteNumber.toString());
+    const currentFavoriteNumber = await contract.retrieve();
+    console.log("Current favorite number:", currentFavoriteNumber.toString());
+    const transactionResponse = await contract.store("7");
+    await transactionResponse.wait(1);
+    const newFavoriteNumber = await contract.retrieve();
+    console.log("New favorite number:", newFavoriteNumber.toString());
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
